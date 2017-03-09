37d12acf91c0f212a9911ea4f6062465d6a1f6d0
Valentin Delaye <valentin.delaye@novaccess.ch>
2017-03-09 18:32:37 +0100

    Update MQTT message to json

--- producer.js
+++ producer.js
@@ -5,8 +5,9 @@ var client  = mqtt.connect('tcp://127.0.0.1:1883', {username : 'iot', password :
 // Send X message on connection on the 'iot' topic and then close the client
 client.on('connect', function () {
 	for(var i = 0 ; i < 100 ; i++) {
-		console.log('Sending message ' + i);
-		client.publish('iot', 'Sending message ' + i);
+		var message = JSON.stringify({message : "Sending message " + i, index : i});
+		console.log("Sending message : " + message);
+		client.publish('iot', message);
 	}
 	client.end();
 })
